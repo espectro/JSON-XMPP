ea46a79d88a302030ba0d4652b34520c3abe3001
Andrey Alexandrov <andrey.aleksandrov@gmail.com>
2013-05-30 11:31:17 +0400

    Add mongodb

--- bc.py
+++ bc.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import sys, re, json, xmpp, pymongo
+from mongoqueue import queue
 
 connection = pymongo.MongoClient("mongodb://localhost")
 db = connection.xmpp           # attach to db
-cnf = db.config         # specify the colllection
-  
+cnf = db.config                # specify the colllection
+
+inbox  = queue(db.inbox)
+outbox = queue(db.outbox)
+
+inbox.clear()
+outbox.timeout(-1)
+
 try:
   cid = cnf.find_one()
   
@@ -23,22 +31,30 @@ sent_message_id = 0
 
 def message_callback(session, message):
   global sent_message_id
+  content = message.getTags('body')
+  if len(content) > 0:
+    try:
+      msg = json.loads(content[0].getData())
+      mime = 'application/json'
+    except:
+      msg = content[0].getData()
+      mime = 'text/plain'
+    m = re.search('(.+)@(.+)/(.+)', str(message.getAttr('from'))) 
+    inbox.add( {
+      "sender"  : { "user" : m.group(1), "domain": m.group(2), "ressource": m.group(3) },
+      "mime"    : mime,
+      "message" : msg
+    })
 
-  gcm = message.getTags('body')
-  if len(gcm) > 0:
-    m = re.search('(.+)@(.+)/(.+)', str(message.getAttr('from')))
-    sender = { "user" : m.group(1), "domain": m.group(2), "ressource": m.group(3) }
-    gcm_json = gcm[0].getData()
+    #send(sender, gcm_json.encode('utf-8'))
 
-    send(sender, gcm_json.encode('utf-8'))
-
-def send(to, json_data):
+def send(to, message):
   template = ("<message from=\"{0}\" to=\"{1}@{2}/{3}\" type=\"chat\"><html xmlns=\"http://jabber.org/protocol/xhtml-im\"><body>{4}</body></html></message>")
   connection.send(xmpp.protocol.Message(
-    node=template.format(connection.Bind.bound[0], to["user"], to["domain"], to["ressource"], json_data)))
+    node=template.format(connection.Bind.bound[0], to["user"], to["domain"], to["ressource"], str(message))))
 
 jid = xmpp.JID(user)
-connection = xmpp.Client(server) #, debug=[]
+connection = xmpp.Client(server, debug=[]) #
 connection.connect()
 auth = connection.auth(jid.getNode(), password, ressource)
 if not auth:
@@ -52,6 +68,9 @@ connection.sendInitPresence()
 i = 1
 while True:
   connection.Process(1)
-  #  print i
+  out = outbox.reserve()
+  if out != None:
+    send(out["sender"],json.dumps(out["message"]))
+    outbox.remove(out)
   i = i + 1
 	
